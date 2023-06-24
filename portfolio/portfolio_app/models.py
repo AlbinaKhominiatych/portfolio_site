6ddc6348567b160c87ba46190d1f6a8d788b0cd4
AlbinaHominyatich <hominyatichalbina@gmail.com>
2023-06-24 15:21:31 +0300

    add project

--- portfolio/portfolio_app/models.py
+++ portfolio/portfolio_app/models.py
@@ -3,4 +3,6 @@ from django.db import models
 class Project(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField()
-    # Додайти інші поля, які потрібні
+
+    def __str__(self):
+        return self.title
