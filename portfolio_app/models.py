24f81e0848814cd2838fac7e5e9801c6374f9167
AlbinaHominyatich <hominyatichalbina@gmail.com>
2023-06-25 15:27:03 +0300

    вернула все до стану додавання експірієгнсу(та хто ці коміти дивитися буде?)

---
+++ portfolio_app/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+class Project(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.title
