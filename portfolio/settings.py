2a83bb52aac7c0d664971dc724d9ddc52392dc32
AlbinaHominyatich <hominyatichalbina@gmail.com>
2023-06-25 18:40:19 +0300

    media for heruko

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -122,6 +122,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 "STATIC_ROOT = os.path.join(BASE_DIR, 'static')"
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'static/media')
 
