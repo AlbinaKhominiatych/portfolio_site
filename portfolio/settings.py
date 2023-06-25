4ad3b524fcf095d36f7e570119030053bd813a77
AlbinaHominyatich <hominyatichalbina@gmail.com>
2023-06-25 16:24:40 +0300

    add MEDIA_ROOT for heruko

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -123,7 +123,8 @@ STATIC_URL = '/static/'
 "STATIC_ROOT = os.path.join(BASE_DIR, 'static')"
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static/media')
+
 
 
 
