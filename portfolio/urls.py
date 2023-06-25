37150f185619c5e1da3a4e73537322486048fd37
AlbinaHominyatich <hominyatichalbina@gmail.com>
2023-06-25 16:23:32 +0300

    add settings.DEBUG for heruko

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -7,3 +7,7 @@ urlpatterns = [
     path('', IndexView.as_view(), name='home'),
     path('projects/', ProjectListView.as_view(), name='project-list'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
